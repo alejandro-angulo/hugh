24767dad8a50f39a627e1321fd2587947e6adace
Alejandro Angulo <iam@alejandr0angul0.dev>
2021-03-07 10:17:16 -0800

    Added basic TUI for lights command

--- cmd/lights/main.go
+++ cmd/lights/main.go
@@ -6,9 +6,11 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"runtime/debug"
 	"time"
 
 	"github.com/alejandro-angulo/hugh/pkg/api"
+	"github.com/rivo/tview"
 )
 
 func main() {
@@ -51,7 +53,96 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for _, light := range lights {
-		fmt.Printf("%s\n", light.Name)
+	app := tview.NewApplication()
+	finder(app, lights)
+	if err := app.Run(); err != nil {
+		fmt.Println(string(debug.Stack()))
+		log.Fatal(err)
 	}
+}
+
+func finder(app *tview.Application, lights []api.Light) {
+	lightsList := tview.NewList()
+	lightsList.SetBorder(true).SetTitle("Lights")
+
+	lightInfo := tview.NewList()
+	lightInfo.SetBorder(true).SetTitle("Light Info")
+	lightInfo.SetDoneFunc(func() {
+		app.SetFocus(lightsList)
+		lightInfo.Clear()
+	})
+
+	flex := tview.NewFlex().
+		AddItem(lightsList, 0, 1, true).
+		AddItem(lightInfo, 0, 1, false)
+
+	getLightStatus := func(light *api.Light) (status string) {
+		status = "Off"
+		if light.State.On {
+			status = "On"
+		}
+
+		return
+	}
+
+	addLightToList := func(i int, light api.Light) {
+		lightsList.AddItem(light.Name, "", 0, func() {
+			lightInfo.Clear()
+
+			lightInfo.AddItem("Active", getLightStatus(&light), 'a', func() {
+				light.ToggleLight()
+				lightInfo.SetItemText(i, "Active", getLightStatus(&light))
+			})
+
+			lightInfo.AddItem("Brightness", fmt.Sprintf("%v", light.State.Brightness), 'b', nil)
+			lightInfo.AddItem("Hue", fmt.Sprintf("%v", light.State.Hue), 'h', nil)
+			lightInfo.AddItem("Saturation", fmt.Sprintf("%v", light.State.Saturation), 's', nil)
+
+			app.SetFocus(lightInfo)
+		})
+	}
+
+	for i, light := range lights {
+		addLightToList(i, light)
+	}
+
+	pages := tview.NewPages().
+		AddPage("someName", flex, true, true)
+	app.SetRoot(pages, true)
+
+	//root := tview.NewTreeNode("Lights")
+	//tree := tview.NewTreeView().SetRoot(root).SetCurrentNode(root)
+
+	//for _, light := range lights {
+	//// fmt.Printf("[%d] %s\n", i, light.Name)
+	//lightNode := tview.NewTreeNode(light.Name).SetReference(light).SetSelectable(true).SetExpanded(false)
+
+	//lightStatus := getLightStatus(&light)
+	//lightStatusNode := tview.NewTreeNode(lightStatus).SetReference(light)
+	//lightStatusNode.SetSelectedFunc(func() {
+	////light := node.GetReference()
+	//// TODO: handle exceptional case when light is none
+	//light.ToggleLight()
+	//lightStatusNode.SetText(getLightStatus(&light))
+	//})
+	//lightNode.AddChild(lightStatusNode)
+
+	//root.AddChild(lightNode)
+	//}
+
+	//tree.SetSelectedFunc(func(node *tview.TreeNode) {
+	//light := node.GetReference()
+	//if light == nil {
+	//return
+	//} else {
+	//node.SetExpanded(!node.IsExpanded())
+	//}
+
+	////children := node.GetChildren()
+	////if len(children) == 0 {
+	////// Toggle expanded
+	////node.SetExpanded(!node.IsExpanded())
+	////} else {
+	////}
+	//})
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/alejandro-angulo/hugh
 go 1.15
 
 require (
+	github.com/gdamore/tcell v1.4.0 // indirect
 	github.com/google/go-cmp v0.5.4
 	github.com/grandcat/zeroconf v1.0.0
+	github.com/rivo/tview v0.0.0-20210217110421-8a8f78a6dd01 // indirect
 )
